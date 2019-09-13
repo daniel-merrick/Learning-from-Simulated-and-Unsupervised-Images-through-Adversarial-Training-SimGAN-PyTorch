15e9c55cb09ec800e48fa269bd339c029720d14d
Daniel Merrick <dmerrick@purdue.edu>
2019-09-13 16:11:18 -0400

    doc

--- simgan/main.py
+++ simgan/main.py
@@ -2,16 +2,19 @@ import sys
 
 sys.path.insert(0, 'lib/')
 
+# Import class and config file for training 
 from train_simgan import TrainSimGAN
 import train_config
 
-''' Still need to implement '''
+# Import class and config file for testing
 from test_simgan import TestSimGAN
 import test_config
 
 if __name__ == '__main__':
-	trainer = TrainSimGAN(train_config)
-	trainer.train()
-
+	# If you want to train, uncomment the following two line
+	#trainer = TrainSimGAN(train_config)
+	#trainer.train()
+	
+	# If you want to test, uncomment the following two lines
 	#tester = TestSimGAN(test_config)
 	#tester.refine()
